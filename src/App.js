64fa039cbde75c5d4930a9130df0abc5e24a265b
cherrymanu <charishmamanupati11@gmail.com>
2022-06-21 20:01:13 +0530

    Working state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
+import logo from "./logo.svg"
+//mock data
+import data from "./data.json";
+//components
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function App() {
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === id ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
   }
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+ 
+ 
+ return (
+   <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
+     <Header />
+     <ToDoList toDoList={toDoList}/>
+     <ToDoForm addTask={addTask}/>
+   </div>
+ );
 }
-
+ 
 export default App;
+
+// class App() {
+  
+//     //const [ toDoList, setToDoList ] = useState(data);
+//     return (
+      
+//       // <div className="App">
+//       //   <div className="App-header">
+//       //     <img src={logo} className="App-logo" alt="logo" />
+//       //     <h2>Hello Reach</h2>
+//       //   </div>
+//       //   <p className="App-intro">
+//       //     To get started, edit <code>src/App.js</code> and save to reload.
+//       //   </p>
+//       // </div>
+//      <div className="App">
+//        <Header /> 
+//       </div>
+
+//     );
+  
+// }
+
+// export default App;
